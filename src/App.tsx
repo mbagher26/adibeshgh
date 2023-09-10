78385f53694d41bf4ce57badfdcfcb09a9beced3
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-09-10 08:34:33 +0330

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Adib from './components/adib'
 
 
+function App() {
 
-export default function App() {
-  
-
-
-
-
- 
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div>
+        <Adib/>
+      </div>
+    </>
+  )
 }
+
+export default App
 
