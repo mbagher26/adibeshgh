3a96f4ebd6bf01a746d0474319725506925b6a4e
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-09-23 13:46:04 +0330

    delete function getCourseSection

--- src/components/coursepublicsection.tsx
+++ src/components/coursepublicsection.tsx
@@ -21,28 +21,11 @@ export default function CoursePublicSections(){
         NewLessons?: number | undefined,
         
       }[]>();
-      const [coursesection,setCoursesection] = useState<{
-        /** Section's id */
-        ID?: number | undefined;
-        /** Section's Title */
-        Title: string;
-        /** Section's CourseId */
-        CourseID?: number | undefined;
-        /** Section's description */
-        Description?: string | undefined;
-        /** Section's exercise count */
-        ExerciseCount?: number | undefined;
-        /** Section's lessons count */
-        LessonCount?: number | undefined;
-        /** Section's exams count */
-        ExamCount?: number | undefined;
-        /** Section's new lessons */
-        NewLessons?: number | undefined; 
-      }[]>();
+
 
       useEffect(() =>{
+
         getCoursePublicSections(location.state.data);
-        getCourseSections(location.state.data);
 
       },[])
 
@@ -51,39 +34,27 @@ export default function CoursePublicSections(){
     api.c.getCoursePublicSections(id).then((res) =>{
 
         console.log('coursepublicsection:',res.data.result);
-        setCoursesection(res.data.result);
+        
         setCoursepublic(res.data.result);
     }).catch((error) =>{
         console.error('Error:',error);
     })
    }
 
-   function getCourseSections(id:number){
-    api.c.getCourseSections(id).then((res) =>{
-        console.log(res.data.result);
-    }).catch((error) =>{
-        console.error('Error:',error);
-    }
-    );
-    
-   }
 
    return(
 
     <div>
-        {
+
+        <p>cousrse public section page</p>
+        {/* {
             coursepublic?.map((item) =>
-                <div >
+                <div>
                     <p>courseid:{item.CourseID}</p>
-                    <p>Description:{item.Description}</p>
-                    <p>Exa:{item.ExamCount}</p>
-                    <p>Exer:{item.ExerciseCount}</p>
+                    
                     <p>ID:{item.ID}</p>
-                    <p>Less:{item.LessonCount}</p>
-                    <p>new:{item.NewLessons}</p>
-                    <p>title:{item.Title}</p>
-
-
+                    
+                    
 
                     <button className="coursepublic">
 
@@ -92,21 +63,15 @@ export default function CoursePublicSections(){
                     <p>{item.Description}</p>
 
                     <div>
-                        <p style={{display: "inline-flex"}}>{item.LessonCount } </p>
-
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book-fill" viewBox="0 0 16 16">
-                        <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
-                        </svg>
+                        <p>{item.LessonCount } </p>
                     </div>
                     
                     </button>
+                     
                 </div>
-
             )
-        }
+        } */}
     </div>
-
    )
-
 }
 
