2b22916ef79af550391d5ac38bd52886bc17a12b
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-09-26 12:13:25 +0330

    fix:change class to className

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (<>
 
-        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
 
     </>)
 }
