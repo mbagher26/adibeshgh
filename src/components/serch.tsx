fba16cf9024b407ebd7b24cbd189fd72b019b184
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-09-26 08:39:50 +0330

    create uesState and useEffect and call function

--- src/components/serch.tsx
+++ src/components/serch.tsx
@@ -1,15 +1,23 @@
+import { useEffect, useState } from 'react';
 import {api} from './adib.tsx';
 
 
-
+const [resultsearch,setResultsearch] = useState();
 
 export default function Search(){
+
+
+    useEffect(() =>{
+        search();
+    },[])
 
 
     function search(){
 
         api.c.search({phrase,index,limit}:{phrase:String,index:Number,limit:Number}).then((res) => {
-            
+
+            setResultsearch(res.data.result);
         })
     }
 }
+
