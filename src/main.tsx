d21e844b253a98d88bedc41cec111839e52b2b2b
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-09-27 09:47:48 +0330

    create route for about

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,24 @@ import Adib from './components/adib';
 import Course from './components/course';
 import "./App.css";
 import CourseSection from './components/coursesections';
-import  ReactDOM  from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter ,createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import React from 'react';
 import Search from './components/search';
-
+import About from './components/about';
+import Header from './components/Header';
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<Adib />,
+    path: '/',
+    element: <Adib />,
   },
   {
-    path:'/home',
-    element:<Navigate replace to='/'/>,
+    path: '/home',
+    element: <Navigate replace to='/' />,
   },
   {
-    path:'/course',
-    element:<Course />,
+    path: '/course',
+    element: <Course />,
     children: [
       {
         path: ":courseId",
@@ -27,26 +28,35 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path:'/search',
-    element:<Search />,
+    path: '/search',
+    element: <Search />,
+  },
+  {
+    path: '/about',
+    element: <About />
   }
 ]);
 
 
 const root = document.getElementById('root');
 
-if(root){
-ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
-);
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <Header />
+        <RouterProvider router={router}/>
+
+      </BrowserRouter>
+      
+    </React.StrictMode>
+  );
 }
 
 
 
-  
-  
+
+
 
 
 
