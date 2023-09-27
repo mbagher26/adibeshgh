9c3c0d301eb6553946fe2e64f62dc554e4ba38a5
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-09-27 10:30:18 +0330

    update import and add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 
-import './App.css'
-import Adib from './components/adib'
+import { Route, RouterProvider, Routes ,createBrowserRouter, Navigate} from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Adib from './components/pages/adib';
+import Course from './components/pages/course';
+import CourseSection from './components/pages/coursesections';
+import Search from './components/pages/search';
+import About from './components/pages/about';
+import React from 'react';
 
 
 function App() {
 
   return (
     <>
+      <div>        
+        <Header/>
+      </div>
       <div>
-        <Adib/>
+        <Routes>
+          <Route path=''/>
+
+           
+        </Routes>       
       </div>
     </>
   )
