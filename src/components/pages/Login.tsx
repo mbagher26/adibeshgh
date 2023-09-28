68db16ffc25b45fd604fd2cd3ee760841e5d52e2
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-09-28 14:03:41 +0330

    move useEffect , pass Input to login ,add class css

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,34 +1,38 @@
 import { Link } from "react-router-dom";
 import {api} from "./adib.tsx";
 import { useEffect } from "react";
+
+
 export default function Login(){
 
+    useEffect(() =>{
+        Mobilelogin();
+    },[])
+
     function Mobilelogin(){
-        
-        api.login.login(mobile:String).then((res) =>{
+        const Input =(document.getElementsByName('EM') [0] as HTMLInputElement).value
+        api.login.login(Input).then((res) =>{
             console.log(res);
         }).catch((error) =>{
             console.error("ERROR:",error);
         })
     }
 
-    useEffect(() =>{
-        
-    },[])
+
     return(
-        <>
-            <div>
+        <div className="container">
+            <div className="sidbarlogin">
                 <img src="https://adibeshgh.com/assets/logo.7df3b54d.svg"/>
-                <input type="text" id='EM' placeholder="شماره تلفن  یا ایمیل"/>
-                <input type="submit" placeholder="ارسال"/>
+                <input type="text" name="EM" placeholder="شماره تلفن  یا ایمیل"/>
+                <input type="submit" value='ارسال'/>
                 <div>
                     <Link to=''>ثبت نام</Link>
                 </div>
             </div>
-            <div>
+            <div className="imagelogin">
                 <img src="https://adibeshgh.com/assets/wallpaper.17038905.png"/>
             </div>
-        </>
+        </div>
     )
 }
 
