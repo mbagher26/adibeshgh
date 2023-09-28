91a2f58953a06e94136d78bbca0836c1b199231e
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-09-28 12:10:26 +0330

    create mobilelogin

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,7 +1,17 @@
+import {api} from "./adib.tsx"
 export default function Login(){
 
-    
+    function Mobilelogin(){
 
+        api.login.login(mobile:String).then((res) =>{
+            console.log(res);
+        });
+    }
+    return(
+        <>
+        
+        </>
+    )
 }
 
 
