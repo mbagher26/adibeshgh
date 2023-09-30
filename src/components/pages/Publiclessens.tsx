92a0fe3130431e86da5e9a771a198eb69d0e7609
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-09-30 13:40:30 +0330

    import use location for get input data from course page

--- src/components/pages/Publiclessens.tsx
+++ src/components/pages/Publiclessens.tsx
@@ -1,19 +1,21 @@
+import { useLocation } from 'react-router-dom';
 import { api } from './adib.js';
 
 
 export default function Publiclessens(){
 
+    const location = useLocation();
+
+    
     function Publiclessens(){
         
         api.c.getPublicLessons({courseId,sectionId}:{courseId:Number,sectionId:Number}).then((res) =>{
 
         })
-
-
     }
     return(
         <>
-            
+
         </>
     )
     
