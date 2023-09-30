3fa9f67ea317f9cb3822450315aa6c881d32fd31
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-09-30 13:26:29 +0330

    feat:create public section page

---
+++ src/components/pages/Publiclessens.tsx
@@ -0,0 +1,20 @@
+import { api } from './adib.js';
+
+
+export default function Publiclessens(){
+
+    function Publiclessens(){
+        
+        api.c.getPublicLessons({courseId,sectionId}:{courseId:Number,sectionId:Number}).then((res) =>{
+
+        })
+
+
+    }
+    return(
+        <>
+            
+        </>
+    )
+    
+}
