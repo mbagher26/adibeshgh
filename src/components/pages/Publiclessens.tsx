d47665b55cff552ea744e03712568c446ce5dc08
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-09-30 14:24:38 +0330

    add useEffect for call Publiclessens function & putting location.state in courseId and sectionId and passing to Publiclessens

--- src/components/pages/Publiclessens.tsx
+++ src/components/pages/Publiclessens.tsx
@@ -1,21 +1,27 @@
 import { useLocation } from 'react-router-dom';
 import { api } from './adib.js';
-
+import { useEffect } from 'react';
 
 export default function Publiclessens(){
 
     const location = useLocation();
+    console.log('2id:',location.state);
+    const courseId = location.state[0];
+    const sectionId = location.state[1];
 
-    
-    function Publiclessens(){
+    useEffect(()=>{
+        Publiclessens({courseId,sectionId});
+    },[])
+
+    function Publiclessens({courseId,sectionId}:{courseId:number,sectionId:number}){
         
-        api.c.getPublicLessons({courseId,sectionId}:{courseId:Number,sectionId:Number}).then((res) =>{
-
+        api.c.getPublicLessons(courseId,sectionId).then((res) =>{
+            console.log('publiclessens:',res.data.result);
         })
     }
     return(
         <>
-
+            <p>thiis is public lessens</p>
         </>
     )
     
