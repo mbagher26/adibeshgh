1d098a7a2a4b697df403bc0429a0a1aa4d8da6ed
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-10-02 12:10:31 +0330

    create courses useState and setCourses

--- src/components/pages/AgeCategoryCourses.tsx
+++ src/components/pages/AgeCategoryCourses.tsx
@@ -1,7 +1,56 @@
 import { api } from './adib.js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function AgeCategoryCourses(){
+
+    interface CourseModel {
+        /** Course id */
+        ID?: number;
+        /** Course Title */
+        Title: string;
+        /** Draft */
+        Draft?: number;
+        /** Number of users who can join the course */
+        Capacity?: number;
+        /** Active status of course */
+        Active?: number;
+        /** Age category id */
+        AgeCategoryID?: number;
+        /** Cover image id */
+        Cover?: string;
+        /** Description */
+        Description?: string;
+        /** Persian End Date */
+        EndDateFa?: string;
+        /** Persian Start Date */
+        StartDateFa?: string;
+        /** Number of course lessons */
+        LessonCount?: number;
+        /** Number of course users */
+        EnrolledCount?: number;
+        /** Enrollment status of the current user */
+        Enrolled?: number;
+        /** AgeCategory Model */
+        AgeCategory?: AgeCategoryModel;
+        /** More inforamtion about the course */
+        InfoList?: {
+          ID?: number;
+          CourseID?: number;
+          Type?: "text" | "tell" | "url";
+          Title?: string;
+          Description?: string;
+        };
+        /** The Link of the course */
+        Link?: string;
+        /** New lessons for current user */
+        NewLessons?: number;
+        /** Privacy of the course */
+        Private?: number;
+        /** Tag */
+        Tag?: string;
+      }
+
+    const [courses,setCourses] = useState<CourseModel[]>();
 
     useEffect(() =>{
         AgeCategoryCourses(6);
@@ -11,11 +60,13 @@ export default function AgeCategoryCourses(){
 
         api.c.getAgeCategoryCourses(AgeCategoryId).then((res) =>{
             console.log(res.data.result);
+            setCourses(res.data.result);
         })
     }
     return(
         <>
             <p>this is page Agecategory</p>
+
         </>
     )
     
