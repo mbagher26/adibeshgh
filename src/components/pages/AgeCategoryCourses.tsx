66aa9ae0bae03dd2802372fb53b2e54c0082f498
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-10-02 11:06:32 +0330

    change file name to AgeCategoryCourses.tsx

---
+++ src/components/pages/AgeCategoryCourses.tsx
@@ -0,0 +1,17 @@
+import { api } from './adib.js';
+import { useEffect } from 'react';
+
+export default function AgeCategoryCourses(){
+
+    useEffect(() =>{
+        AgeCategoryCourses(6);
+    },[])
+
+    function AgeCategoryCourses(AgeCategoryId:number){
+
+        api.c.getAgeCategoryCourses(AgeCategoryId).then((res) =>{
+            console.log(res.data.result);
+        })
+    }
+    
+}
