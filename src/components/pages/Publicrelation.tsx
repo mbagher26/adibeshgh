cc17708d92af9581774cd1ec2f2e7f8ff7b68ca9
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-10-04 10:25:49 +0330

    feat:create Publicrelation component

---
+++ src/components/pages/Publicrelation.tsx
@@ -0,0 +1,18 @@
+
+
+export default function Publicrelation(){
+
+    return(
+        <>
+            <div className='fatherrelation'>
+                {/* <div className='bgClick'></div> */}
+                <div className='publickrelation'>
+                    <p>روابط عمومی</p>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus atque magni quis exercitationem explicabo iste velit ex at inventore iusto, ea sint assumenda! Fugiat in aut, excepturi voluptatibus nesciunt suscipit?</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
