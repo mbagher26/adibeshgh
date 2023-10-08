6d2e2b5a3bad14c7a6e92e6ccbff8d9d48e15e65
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-10-08 08:57:52 +0330

    feat: create Gtouping

---
+++ src/components/pages/Grouping.tsx
@@ -0,0 +1,14 @@
+import {api} from './adib.js';
+
+export default function Grouping(){
+
+    api.c.getCategorizedCourseList().then(() =>{
+
+    })
+    return(
+        <>
+
+        </>
+    )
+}
+
