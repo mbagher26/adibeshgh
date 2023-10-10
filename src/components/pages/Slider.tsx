2be531d633fc5a5c25849b80d7d1623c0a0c58f8
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-10-10 09:36:48 +0330

    deleet import slider.css

--- src/components/pages/Slider.tsx
+++ src/components/pages/Slider.tsx
@@ -1,6 +1,5 @@
 import { useEffect,useState } from "react";
 import { api } from './adib.js';
-// import './Slider.css';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
@@ -69,40 +68,3 @@ export default function SlideComponent() {
         </>
     );
 }
-
-
-
-
-
-// sld_19_JawjrjOz.png
-// sld_26_ydpriUg9.png
-
-
-    // let baner1 ="sld_19_JawjrjOz.png";
-    // let baner2 ="sld_26_ydpriUg9.png";
-
-
-
-//     <Slider> 
-
-//     <div style={{width:'250px'}}>
-//         <p>250</p>
-//         <img src={returnPictureUrl(baner2)}  />
-//     </div>
-
-//     <div style={{width:'130px'}}>
-//         <img src={returnPictureUrl(baner1)} />
-//         <p>130</p>
-//     </div>
-
-//     <div style={{width:'300px'}}>
-//         <img src={returnPictureUrl(baner2)}  />
-//         <p>300</p>
-//     </div>
-
-//     <div style={{width:'100px'}}>
-//         <img src={returnPictureUrl(baner1)} />
-//         <p>100</p>
-//     </div>
-    
-// </Slider>
