a8c6ab76316720b247008d1e2d944c557be31b39
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-11-06 22:07:37 +0330

    update...

--- src/components/pages/search.tsx
+++ src/components/pages/search.tsx
@@ -1,57 +1,58 @@
-import { useState, createContext, useContext } from 'react';
+import { useState} from 'react';
 import {api} from './adib.js';
-import Loading from './Loading.js';
-// import {setSearchResult} from '../../searchSlice.js';
-// import { useDispatch,useSelector } from 'react-redux';
-// import { RootState } from '../../store.ts';
+// import Loading from './Loading.js';
+import type { RootState } from '../redux/store.tsx';
+import { setSearchResult } from '../redux/searchSlice.tsx';
+import { useDispatch,useSelector } from 'react-redux';
 import '../../App.css';
+
+
 interface SearchModel {
     /** Lesson id */
-    ID?: number |undefined;
+    ID?: number;
     /** Lesson Title */
-    Title?: string |undefined;
+    Title?: string;
     /** Lesson Date */
-    Date?: string |undefined;
+    Date?: string;
     /** Lesson Description */
-    Description?: string |undefined;
+    Description?: string;
     /** Content Title */
-    ContentTitle?: string |undefined;
+    ContentTitle?: string;
     /** Content Text */
-    ContentText?: string |undefined;
+    ContentText?: string;
     /** Section ID */
-    SectionID?: number |undefined;
+    SectionID?: number;
     /** Section Title */
-    SectionTitle?: string |undefined;
+    SectionTitle?: string;
     /** Section Description */
-    SectionDescription?: string |undefined;
+    SectionDescription?: string;
     /** Course ID */
-    CourseID?: number |undefined;
+    CourseID?: number;
     /** Course Title */
-    CourseTitle?: string |undefined;
+    CourseTitle?: string;
     /** Course Description */
-    CourseDescription?: string |undefined;
-}
-
+    CourseDescription?: string;
+  }
+  
 export default function Search(){
     
+    const dispatch = useDispatch();
+    const selector = useSelector((state:RootState)=> state.search);
     const [resultsearch,setResultsearch] = useState<SearchModel[]|undefined>([]);
-    // const dispatch = useDispatch();
-    // const Selector = useSelector((state:RootState)=> state.seatch);
-    
-    const [loading,setLoading] = useState<boolean>(false);
+    // console.log(selector);
+    // const [loading,setLoading] = useState<boolean>(false);
     
     
     function search(){
         const phraseInput = (document.getElementsByName('phrase')[0] as HTMLInputElement).value;
-        setLoading(true)
+        // setLoading(true)
         api.c.search(phraseInput, 0, 10).then((res) => {
             
+            console.log('search:',res.data.result);
             setResultsearch(res.data.result);
-            // dispatch(setSearchResult(res.data.result));
-            // let SearchData = createContext<SearchModel[] | undefined>(res.data.result);
+            dispatch(setSearchResult(resultsearch?.map((item) => {item.ContentTextxxx })));
            
-            setLoading(false);
-            console.log('search:',res.data.result)
+            // setLoading(false);
         })
     }
 
@@ -61,13 +62,19 @@ export default function Search(){
         <>           
             <input className='input-search' type='text'  name='phrase'/>
             <input className='input-button-search' type='button' value='جستجو' onClick={search}/>
-            {/* {Selector.ID}
-            {Selector.Title} */}
+            {selector.ID}
+            {selector.Title}
+            {selector.CourseDescription}
+            {selector.CourseID}
+            {selector.CourseTitle}
+            {selector.SectionID}
+            {selector.ContentText}
+            {selector.ContentTitle}
             <br/>
-            {loading?<Loading/>:            
+            {/* {loading?<Loading/>:            
                 resultsearch?.map((item) => 
                 <div key={item.ID}>{item.Title}</div>
-            )}
+            )} */}
         </>
     )
 }
