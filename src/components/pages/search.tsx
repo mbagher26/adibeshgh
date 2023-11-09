30800f280e3965538a5dc15a88598f27772c8f34
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-11-09 12:39:16 +0330

    delete selector

--- src/components/pages/search.tsx
+++ src/components/pages/search.tsx
@@ -1,5 +1,5 @@
 import { api } from './adib.js';
-import { useAppDispatch, useAppSelector } from '../redux/hooks.ts';
+import { useAppDispatch } from '../redux/hooks.ts';
 import { setSearchResult } from '../redux/searchSlice.tsx';
 import '../../App.css';
 import { useState } from 'react';
@@ -9,7 +9,6 @@ import { useState } from 'react';
 export default function Search() {
     
     const dispatch = useAppDispatch();
-    const selector = useAppSelector((state) => state.search);
     const [phraseInput,setPhraseInput] = useState('');
     const onChange = (event:any) => {
         setPhraseInput(event.target.value)
@@ -34,19 +33,6 @@ export default function Search() {
             
             <input className='input-search' type='text' value={phraseInput} onChange={onChange} />
             <input className='input-button-search' type='button' value='جستجو' onClick={search} />
-            
-            
-
-            {selector.map((item) => 
-                <div key={item.ID}>
-                    
-                    <h1 style={{color: 'white'}}>{item.ContentText}</h1>
-                    <h1 style={{color:'white'}}>{item.ContentTitle}</h1>
-                    <h1>{item.CourseDescription}</h1>
-                    <h1>{item.CourseID}</h1>
-                    <h1 style={{color: 'white'}}>{item.CourseTitle}</h1>
-                </div>
-            )}
             
         </>
     )
