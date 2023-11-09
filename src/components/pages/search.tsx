a8c0ee392f6e8148567f9e334822b1834d0bd43d
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-11-09 10:20:19 +0330

    update search.tsx

--- src/components/pages/search.tsx
+++ src/components/pages/search.tsx
@@ -1,32 +1,26 @@
 import { api } from './adib.js';
-import type { RootState } from '../redux/store.tsx';
+import { useAppDispatch, useAppSelector } from '../redux/hooks.ts';
 import { setSearchResult } from '../redux/searchSlice.tsx';
-import { useDispatch, useSelector } from 'react-redux';
 import '../../App.css';
 
 
 
-const selector = useSelector((state: RootState) => state.search);
-
 export default function Search() {
-
-    const dispatch = useDispatch();
-    console.log({selector});
-
-
+    
+    const dispatch = useAppDispatch();
+    const selector = useAppSelector((state) => state.search)
+    console.log('selector:',selector);
+    
     function search() {
         const phraseInput = (document.getElementsByName('phrase')[0] as HTMLInputElement).value;
         api.c.search(phraseInput, 0, 10).then(
             
             (res) => {
-
                 const resultsearch= res.data.result;
                 console.log('search:', resultsearch)
                 if(resultsearch){
                     console.log({resultsearch});
                     dispatch(setSearchResult(resultsearch))
-                }else{
-                    dispatch(setSearchResult([]))
                 }
             }
         );
@@ -36,6 +30,7 @@ export default function Search() {
         <>
             <input className='input-search' type='text' name='phrase' />
             <input className='input-button-search' type='button' value='جستجو' onClick={search} />
+            
             {selector.map((item) => {
                 <div>
                     <h1 style={{color: 'white'}}>{item.ContentText}</h1>
