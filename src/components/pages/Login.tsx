2d1e94d1daad9ce9686d37b2cebe74c27c6b91a1
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-11-11 13:29:07 +0330

    update Login.tsx file

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,30 +1,38 @@
 import { Link } from "react-router-dom";
 import {api} from "./adib.tsx";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Login(){
-
+    
     useEffect(() =>{
         Mobilelogin();
     },[])
-
+    
+    const handelInput = (event:any) => {
+        setMobile(event.target.value);
+    }
+    const [mobile, setMobile] = useState('');
+    
     function Mobilelogin(){
-        const Input =(document.getElementsByName('EM') [0] as HTMLInputElement).value
-        api.login.login(Input).then((res) =>{
+        
+        console.log(mobile);
+        // const Input =(document.getElementsByName('EM') [0] as HTMLInputElement).value
+        api.login.login(mobile).then((res) =>{
             console.log(res);
-        }).catch((error) =>{
-            console.error("ERROR:",error);
         })
+        // .catch((error) =>{
+        //     console.error("ERROR:",error);
+        // })
     }
 
 
     return(
-        <div className="container">
+        <div className="login-container">
             <div className="sidbarlogin">
                 <img src="https://adibeshgh.com/assets/logo.7df3b54d.svg"/>
-                <input type="text" name="EM" placeholder="شماره تلفن  یا ایمیل"/>
-                <input type="submit" value='ارسال'/>
+                <input type="text" placeholder="شماره تلفن  یا ایمیل" value={mobile} onChange={handelInput}/>
+                <input type="submit" value='ارسال' onClick={Mobilelogin}/>
                 <div>
                     <Link to=''>ثبت نام</Link>
                 </div>
